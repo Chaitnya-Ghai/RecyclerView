b1d3a228c66c60f330cb55f06beefad12c4c2c31
Chaitnya-Ghai <chaitnyaghai2004@gmail.com>
2024-07-02 09:34:41 +0530

    Initial commit

---
+++ app/src/main/java/cg/tutorials/recyclerview/MainActivity.kt
@@ -0,0 +1,43 @@
+package cg.tutorials.recyclerview
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import cg.tutorials.recyclerview.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+    private var data = 10
+    lateinit var binding: ActivityMainBinding
+    lateinit var linearLayoutManager: LinearLayoutManager
+    lateinit var recyclerAdapter: RecyclerAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // Initialize the adapter and RecyclerView
+        recyclerAdapter = RecyclerAdapter(data)
+        linearLayoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = linearLayoutManager
+        binding.recyclerView.adapter = recyclerAdapter
+
+        binding.faBtn.setOnClickListener {
+            data += 1 // Increment the data
+            recyclerAdapter.updateData(data) // Update the adapter data
+            Log.e(TAG, "onCreate: total = $data")
+        }
+    }
+}
---
+++ app/src/main/java/cg/tutorials/recyclerview/RecyclerAdapter.kt
@@ -0,0 +1,33 @@
+package cg.tutorials.recyclerview
+
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+private val TAG = "RecyclerAdapter"
+class RecyclerAdapter(var data: Int) : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        Log.d(TAG, "getItemCount: $data")
+        return data
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    }
+
+    // Method to update data
+    fun updateData(newData: Int) {
+        data = newData
+        notifyDataSetChanged() // Notify the adapter that the data has changed
+    }
+}
